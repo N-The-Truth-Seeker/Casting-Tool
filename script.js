2a1670638933db185e1f72d7f38ff3b46fc53778
N-The-Truth-Seeker <134479485+N-The-Truth-Seeker@users.noreply.github.com>
2023-05-24 15:42:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var players = [];
+var currentIndex = 0;
+var timer;
+
+function startGame() {
+  players = []; // プレイヤーの配列をリセット
+  currentIndex = 0; // 現在のインデックスをリセット
+  
+  var playerCount = parseInt(document.getElementById('playerCount').value);
+  for (var i = 0; i < playerCount; i++) {
+    players.push('探究者');
+  }
+  var werewolfIndex = Math.floor(Math.random() * playerCount);
+  players[werewolfIndex] = '嘘つき';
+  displayRole();
+  document.getElementById('nextButton').disabled = false;
+}
+
+function displayRole() {
+    var resultDiv = document.getElementById('result');
+    var currentPlayer = players[currentIndex];
+    
+    var roleText = document.createElement('span');
+    roleText.textContent = 'プレイヤー ' + (currentIndex + 1) + ' は「' + currentPlayer + '」です。';
+  
+    if (currentPlayer === '嘘つき') {
+      roleText.classList.add('lying');
+    } else if (currentPlayer === '探究者') {
+      roleText.classList.add('explorer');
+    }
+  
+    resultDiv.innerHTML = '';
+    resultDiv.appendChild(roleText);
+  
+    timer = setTimeout(function() {
+      hideRole();
+      showNextMessage();
+    }, 5000);
+}
+
+function hideRole() {
+  document.getElementById('result').innerHTML = '';
+}
+
+function showNextMessage() {
+  var resultDiv = document.getElementById('result');
+  if (currentIndex < players.length - 1) {
+    resultDiv.innerHTML = '次の人に渡してください。';
+  } else {
+    resultDiv.innerHTML = '配役終了';
+    document.getElementById('nextButton').disabled = true;
+  }
+}
+
+function nextPlayer() {
+  currentIndex++;
+  if (currentIndex < players.length) {
+    clearTimeout(timer);
+    displayRole();
+  }
+}
