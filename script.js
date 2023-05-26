4325b9c0e7082c2e3fa4dee4d19d902b7df0bb1a
N-The-Truth-Seeker <134479485+N-The-Truth-Seeker@users.noreply.github.com>
2023-05-26 11:37:26 +0900

    Update script.js
    配役中に配役開始ボタンを押せないように変更。

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var players = [];
 var currentIndex = 0;
 var timer;
+var gameStarted = false; // 配役が開始されたかどうかのフラグ
 
 function startGame() {
+  if (gameStarted) {
+    return; // 配役が既に開始されている場合は何もしない
+  }
+  
+  gameStarted = true; // 配役開始フラグを立てる
+  
   players = []; // プレイヤーの配列をリセット
   currentIndex = 0; // 現在のインデックスをリセット
   
@@ -14,28 +21,30 @@ function startGame() {
   players[werewolfIndex] = '嘘つき';
   displayRole();
   document.getElementById('nextButton').disabled = false;
+  document.getElementById('startButton').disabled = true; // 配役開始ボタンを無効化する
 }
 
+
 function displayRole() {
-    var resultDiv = document.getElementById('result');
-    var currentPlayer = players[currentIndex];
-    
-    var roleText = document.createElement('span');
-    roleText.textContent = 'プレイヤー ' + (currentIndex + 1) + ' は「' + currentPlayer + '」です。';
+  var resultDiv = document.getElementById('result');
+  var currentPlayer = players[currentIndex];
   
-    if (currentPlayer === '嘘つき') {
-      roleText.classList.add('lying');
-    } else if (currentPlayer === '探究者') {
-      roleText.classList.add('explorer');
-    }
-  
-    resultDiv.innerHTML = '';
-    resultDiv.appendChild(roleText);
-  
-    timer = setTimeout(function() {
-      hideRole();
-      showNextMessage();
-    }, 4000);
+  var roleText = document.createElement('span');
+  roleText.textContent = 'プレイヤー ' + (currentIndex + 1) + ' は「' + currentPlayer + '」です.';
+
+  if (currentPlayer === '嘘つき') {
+    roleText.classList.add('lying');
+  } else if (currentPlayer === '探究者') {
+    roleText.classList.add('explorer');
+  }
+
+  resultDiv.innerHTML = '';
+  resultDiv.appendChild(roleText);
+
+  timer = setTimeout(function() {
+    hideRole();
+    showNextMessage();
+  }, 4000);
 }
 
 function hideRole() {
@@ -49,6 +58,8 @@ function showNextMessage() {
   } else {
     resultDiv.innerHTML = '配役終了';
     document.getElementById('nextButton').disabled = true;
+    gameStarted = false; // 配役終了時にフラグをリセットして再び配役開始ボタンを押せるようにする
+    document.getElementById('startButton').disabled = false;
   }
 }
 
